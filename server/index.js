d8d00ea713979b3f2388a49deb52f8c19101f1c9
Varshil Shah <varshilshah.in@gmail.com>
2024-02-03 13:02:09 +0530

    Implement cors to resolve access issues

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
 
 dotenv.config({
   path: "./.env",
 });
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const authRoutes = require("./routes/auth.route");
@@ -21,7 +24,16 @@ mongoose
     console.log(err);
   });
 
+app.use(morgan("dev"));
+
 app.use(authRoutes);
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Page not found",
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -9,13 +9,13 @@ router.post("/api/signup", async (req, res) => {
     let user = await User.findOne({ email });
     if (!user) {
       user = await User.create({ name, email, profilePic });
-      res.status(201).json({
+      res.status(200).json({
         status: "success",
         user,
       });
     }
   } catch (error) {
-    res.status(400).json({
+    res.status(500).json({
       status: "fail",
       error,
     });
